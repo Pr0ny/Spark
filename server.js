230338ec711a3f875210236c11c7346a8ec089b5
Pr0ny <mimoone.ml@gmail.com>
2016-12-05 17:57:59 +0100

    trying to correct some bug

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ var lol = io.sockets.on('connection', function(client)
           //console.log("yolo  =  " + _dataStreamers[j]['sn'] + " " + _dataStreamers[j]['ic']);
           if (_dataStreamers[j]['sn'] == spl[i])
           {
-            //console.log("added to the data send : " + _dataStreamers[j]['sn'] + " " + _dataStreamers[j]['ic']);
+            console.log("added to the data send : " + _dataStreamers[j]['sn'] + " " + _dataStreamers[j]['ic']);
             var obj = {
             sn: spl[i],
             ic: _dataStreamers[j]['ic'],
