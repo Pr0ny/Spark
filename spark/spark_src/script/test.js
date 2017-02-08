1e35958a3abd907cf6ecae306623619eb1de9a24
chemin_q <quentin.chemin@epitech.eu>
2017-02-08 18:37:15 +0100

    debug

--- spark/spark_src/script/test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:false,
-    margin:-50,
-    nav:true,
-    items:1,
-    navText: ["<img class='fleche1' src='css/img/fleche.png'>","<img class='fleche2' src='css/img/fleche_2.png'>"],
-    });
-});
